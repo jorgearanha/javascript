8baf4aa278f48c3d72d30ba0f19ccb6285455254
Jorge Luis Neves Aranha - JAA <jorge.aranha@iteris.com.br>
2019-12-12 22:54:28 -0300

    Problema 1160

--- URI/1160.js
+++ URI/1160.js
@@ -1,20 +1,37 @@
 var input = require('fs').readFileSync('./entrada.txt', 'utf8');
-var lines = input.split('\r\n');
+var lines = input.split('\n');
 
 function obj(str) {
     let arrStr = str.split(' ');
-    this.pa = arrStr[0];
-    this.pb = arrStr[1];
-    this.g1 = arrStr[2];
-    this.g2 = arrStr[3];
+    this.pa = parseInt(arrStr[0]);
+    this.pb = parseInt(arrStr[1]);
+    this.g1 = parseFloat(arrStr[2]);
+    this.g2 = parseFloat(arrStr[3]);
 }
 
-function crescPop() {
-    for (let i = 1; i < lines[0]; i++) {   
-        
-        console.log(obj(lines(i)));
-         
+function crescPop(instancia) {
+    let anos = 0;
+
+    while (instancia.pa < instancia.pb && anos <= 100) {
+        instancia.pa = parseInt(instancia.pa + instancia.pa * (instancia.g1/100));
+        instancia.pb = parseInt(instancia.pb + instancia.pb * (instancia.g2/100));
+        anos++;
+    }
+    
+    return anos;
+}
+
+let num = parseInt(lines[0]);
+
+for (let i = 1; i <= num; i++) {
+    let line = lines[i];
+    
+    let instancia = new obj(line);
+    let anos = crescPop(instancia);
+
+    if (anos > 100) {
+        console.log('Mais de 1 seculo. ');
+    } else {
+        console.log(parseInt(anos) + ' anos. ');
     }
 }
-
-crescPop()
