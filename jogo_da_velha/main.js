f5f481c3423d5bce6b1c7764e7fd7497e28df555
Jorge Luis Neves Aranha - JAA <jorge.aranha@iteris.com.br>
2019-12-15 20:14:23 -0300

    Logica do tabuleiro funcionando

--- jogo_da_velha/main.js
+++ jogo_da_velha/main.js
@@ -1,38 +1,90 @@
+const panettone = {
+    elem: document.createElement('img'),
+    nome: 'panettone'
+};
+panettone.elem.src = 'images/panettone.png';
+panettone.elem.className = 'pane ttones';
+
+const chocottone = {
+    elem: document.createElement('img'),
+    nome: 'chocottone'
+};
+chocottone.elem.src = 'images/chocottone.png';
+chocottone.elem.className = 'choco ttones';
+
+var ttone = panettone;
 
 function main() {
 
-    let linha1 = document.getElementsByClassName('linha1').item(0);
-    let linha2 = document.getElementsByClassName('linha2').item(0);
-    let linha3 = document.getElementsByClassName('linha3').item(0);
+    let coluna1 = document.getElementsByClassName('linha1').item(0);
+    let coluna2 = document.getElementsByClassName('linha2').item(0);
+    let coluna3 = document.getElementsByClassName('linha3').item(0);
 
     // Construindo um Array de linhas para formar o tabuleiro
-    let tabuleiro = [linhaToArray(linha1), linhaToArray(linha2), linhaToArray(linha3)];
+    let tabuleiro = [montaLinha(coluna1), montaLinha(coluna2), montaLinha(coluna3)];
 
     clickTabuleiro(tabuleiro);
-    
 }
 
-function clickTabuleiro(tabuleiro){
+function clickTabuleiro(tabuleiro) {
     for (let i = 0; i < tabuleiro.length; i++) {
-        for (let j = 0; j < tabuleiro[i].length; j++){
-            tabuleiro[i][j].addEventListener('click', () => {
-                let elem = document.createElement('img');
-                elem.src = 'images/Chocottone.png';
-                elem.className = 'ttones';
-                console.log(elem);
-                
-                tabuleiro[i][j].appendChild(elem);
-            });
+        for (let j = 0; j < tabuleiro[i].length; j++) {
+            // Cria a arrow function referenciada em evento para poder ser removida após o primeiro click.
+            let evento = () => {
+                // Executa o evento esperado ao clicar no tabuleiro.
+                let clone = ttone.elem.cloneNode(true);
+                tabuleiro[i][j].elem.appendChild(clone);
+                tabuleiro[i][j].ttone = ttone.nome;
+                // Impede o evento de ocorrer novamente após o primeiro click.
+                tabuleiro[i][j].elem.removeEventListener('click', evento);
+                let vencedor = verificaVencedor(tabuleiro);
+                if (vencedor) alert('O vencedor foi: ' + vencedor);
+                if (verificaVelha(tabuleiro)) alert('Deu velha');
+                // Altera o ttone a cada rodada.
+                alteraTtone();
+            }
+            tabuleiro[i][j].elem.addEventListener('click', evento);
         }
     }
 }
 
-function linhaToArray(linha) {
-    let arrLinha = [];
-    for (let index = 1; index <= 3; index++) {
-        arrLinha.push(linha.getElementsByClassName('coluna' + index).item(0))
+function verificaVencedor(tab) {
+    for (let i = 0; i < 3; i++) {
+        if (
+            (((tab[i][0].ttone && tab[i][1].ttone) && tab[i][2].ttone) && ((tab[i][0].ttone ==  tab[i][1].ttone) && (tab[i][0].ttone == tab[i][2].ttone))) ||
+            (((tab[0][i].ttone && tab[1][i].ttone) && tab[2][i].ttone) && ((tab[0][i].ttone ==  tab[1][i].ttone) && (tab[0][i].ttone == tab[2][i].ttone)))
+        ) return tab[i][i].ttone;
+        else if(
+        (((tab[0][0].ttone && tab[1][1].ttone) && tab[2][2].ttone) && ((tab[0][0].ttone ==  tab[1][1].ttone) && (tab[0][0].ttone == tab[2][2].ttone))) ||
+        (((tab[2][0].ttone && tab[1][1].ttone) && tab[0][2].ttone) && ((tab[2][0].ttone ==  tab[1][1].ttone) && (tab[2][0].ttone == tab[0][2].ttone)))
+        ) return tab[1][1].ttone;
     }
-    return arrLinha;
+}
+
+function verificaVelha(tab) {
+    let boo = true;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            boo = boo && tab[i][j].ttone;
+        }
+    }
+    return boo;
+}
+
+function alteraTtone() {
+    if (ttone == panettone) ttone = chocottone;
+    else ttone = panettone;
+}
+
+function montaLinha(coluna) {
+    let arrColuna = [];
+    for (let index = 1; index <= 3; index++) {
+        arrColuna.push({
+            elem: coluna.getElementsByClassName('coluna' + index).item(0),
+            ttone: ''
+        });
+    }
+    return arrColuna;
 }
 
 document.addEventListener("DOMContentLoaded", main);
